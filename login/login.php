fee9258ef3e8f010b430fd9252497134ffc46b51
Nanja <nanja.suepsang@payrexx.com>
2024-03-27 15:06:12 +0100

    22-03-2024: Create a Log in Form

---
+++ login/login.php
@@ -0,0 +1,11 @@
+<?php
+
+//$servername = "localhost:3036";
+//$username = "minimal-dev-php";
+//$password = "Minimal24";
+//
+//$database = new mysqli($servername, $username, $password);
+//
+//if (! $database) {
+//    die("Cannot create a database");
+//}
