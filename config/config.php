2ea05e0ab2729b85edbb14be332c14e2dc34ebcb
Nanja <nanja.suepsang@payrexx.com>
2024-03-28 14:14:08 +0100

    28-03-2024: JSON schreiben

---
+++ config/config.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    'products' => [
+        [
+            'name' => 'Product 1',
+            'categories' => '',
+            'product_information' => '',
+            'quantity' => 50,
+            'price_pro_product' => 100,
+            'free_shipping' => true
+        ],
+        [
+            'name' => 'Product 2',
+            'categories' => '',
+            'product_information' => '',
+            'quantity' => 30,
+            'price_pro_product' => 50,
+            'free_shipping' => true
+        ],
+        [
+            'name' => 'Product 3',
+            'categories' => '',
+            'product_information' => '',
+            'quantity' => 50,
+            'price_pro_product' => 20,
+            'free_shipping' => true
+        ],
+        [
+            'name' => 'Product 4',
+            'categories' => '',
+            'product_information' => '',
+            'quantity' => 100,
+            'price_pro_product' => 45,
+            'free_shipping' => true
+        ],
+        [
+            'name' => 'Product 5',
+            'categories' => '',
+            'product_information' => '',
+            'quantity' => 30,
+            'price_pro_product' => 78,
+            'free_shipping' => true
+        ]
+    ]
+];
